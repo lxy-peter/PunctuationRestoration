126c835dab6b728046264e2bf7b9e91fbc12a681
Xing Yi Liu <liuxingyi99@gmail.com>
2024-01-15 09:47:45 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from models.multimodal import *
-from models.text import *
-
-SUPPORTED_MODELS = {
-    'EfficientPunct': EfficientPunct,
-    'EfficientPunctBERT': EfficientPunctBERT,
-    'EfficientPunctTDNN': EfficientPunctTDNN,
-}
